6547ebd2aebe46d132a50e4b732854c9a2453e39
philippa muston <philippa@thefusionworks.com>
2019-11-24 13:07:40 +0000

    Code updated to match generics course

--- app/src/main/java/com/phil/mylabdaemon_v3/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/phil/mylabdaemon_v3/ui/auth/LoginFragment.kt
@@ -2,17 +2,24 @@ package com.phil.mylabdaemon_v3.ui.auth
 
 import android.os.Bundle
 import android.util.Log
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+
 import com.phil.mylabdaemon_v3.R
+import com.phil.mylabdaemon_v3.models.AuthToken
+import com.phil.mylabdaemon_v3.ui.auth.state.AuthStateEvent
+import com.phil.mylabdaemon_v3.ui.auth.state.AuthStateEvent.*
 import com.phil.mylabdaemon_v3.ui.auth.state.LoginFields
-import com.phil.mylabdaemon_v3.util.GenericApiResponse
+import com.phil.mylabdaemon_v3.util.ApiEmptyResponse
+import com.phil.mylabdaemon_v3.util.ApiErrorResponse
+import com.phil.mylabdaemon_v3.util.ApiSuccessResponse
 import kotlinx.android.synthetic.main.fragment_login.*
 
-class LoginFragment : BaseAuthFragment()  {
+
+class LoginFragment : BaseAuthFragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,21 +31,27 @@ class LoginFragment : BaseAuthFragment()  {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Log.d(TAG, "LoginFragment: ${viewModel.hashCode()}")
-
+        Log.d(TAG, "LoginFragment: ${viewModel}")
 
         subscribeObservers()
+
+        login_button.setOnClickListener {
+            viewModel.setAuthToken(
+                AuthToken(
+                    1,
+                    "gdfngidfng4nt43n43jn34jn"
+                )
+            )
+        }
     }
 
-    fun subscribeObservers() {
-        viewModel.viewState.observe(viewLifecycleOwner, Observer {
-            it.loginFields?.let{loginFields ->
 
-                loginFields.login_email?.let{input_email.setText(it)}
-                loginFields.login_password?.let{input_password.setText(it)}
-
+    fun subscribeObservers(){
+        viewModel.viewState.observe(viewLifecycleOwner, Observer{
+            it.loginFields?.let{
+                it.login_email?.let{input_email.setText(it)}
+                it.login_password?.let{input_password.setText(it)}
             }
-
         })
     }
 
@@ -51,4 +64,6 @@ class LoginFragment : BaseAuthFragment()  {
             )
         )
     }
+
 }
+
